fcf307459ae6eaccbda687fc4bec26845d7284ca
Igor Montagner <igordsm@gmail.com>
2020-08-13 12:18:06 -0300

    Deployed 761c127 with MkDocs version: 1.1.2

---
+++ aulas/01-introducao/t4.py
@@ -0,0 +1,10 @@
+import numpy as np
+import numpy.random
+import sys
+
+n = int(input())
+arr = np.random.rand(n)
+print(10)
+print('\n'.join([str(a) for a in arr]))
+
+print(f'{np.mean(arr):0.9}', f'{np.var(arr):0.9}', file=sys.stderr)
