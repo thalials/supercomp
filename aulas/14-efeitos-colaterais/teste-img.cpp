b3902a4066b5e33e1f38196a9847da23551c3b83
Igor Montagner <igordsm@gmail.com>
2020-10-29 09:55:38 -0300

    Deployed 00751b7 with MkDocs version: 1.1.2

---
+++ aulas/14-efeitos-colaterais/teste-img.cpp
@@ -0,0 +1,26 @@
+#include "imagem.hpp"
+#include <iostream>
+#include <random>
+#include <omp.h>
+
+int main(int argc, char *argv[]) {
+    Imagem input = Imagem::read(std::string(argv[1]));
+    Imagem output(input.rows, input.cols);
+
+    auto vec = new std::default_random_engine[omp_get_max_threads()];
+    std::uniform_int_distribution<int> gen(1, 11);
+
+    #pragma omp parallel for schedule(static)
+    for (int k = 0; k < input.total_size; k++) {
+        int r = gen(vec[omp_get_thread_num()]);
+        if (r == 1) {
+            input.pixels[k] = 0;
+        } else if (r == 10) {
+            input.pixels[k] = 255;
+        }
+    }
+
+    // adicionar ruido aqui
+    input.write(std::string(argv[2]));
+    return 0;
+}
