43cb0e43588b094307528d92c84655629ecf1f53
Igor Montagner <igordsm@gmail.com>
2021-04-29 19:19:00 -0300

    Deployed 60e4267 with MkDocs version: 1.1.2

--- aulas/13-paralelismo-tarefas-dados/exemplo2.cpp
+++ aulas/13-paralelismo-tarefas-dados/exemplo2.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 
 int main() {
-    #pragma omp parallel for schedule(dynamic, 4)
-    for (int i = 0; i < 16  ; i++) {
+    #pragma omp parallel for schedule(static, 4)
+    for (int i = 0; i < 20; i++) {
         std::cout << i << " Eu rodei na thread: " << omp_get_thread_num() << "\n";
     }
     return 0;
--- aulas/13-paralelismo-tarefas-dados/pi-numeric-integration.cpp
+++ aulas/13-paralelismo-tarefas-dados/pi-numeric-integration.cpp
@@ -5,38 +5,21 @@ static long num_steps = 1000000000;
 double step;
 
 int main() {
-    double pi, sum1 = 0.0, sum2 = 0;
+    int i;
+    double pi, sum = 0.0;
     step = 1.0 / (double)num_steps;
 
     auto start_time = std::chrono::high_resolution_clock::now();
-
-    #pragma omp parallel 
-    {
-        #pragma omp master
-        {
-            #pragma omp task
-            {
-                for (int i = 0; i < num_steps/2; i++) {
-                    double x = (i + 0.5) * step;
-                    sum1 += 4.0 / (1.0 + x * x);
-                }
-            }
-            #pragma omp task
-            {
-                for (int i = num_steps/2; i < num_steps; i++) {
-                    double x = (i + 0.5) * step;
-                    sum2 += 4.0 / (1.0 + x * x);
-                }
-            }
-        }
-        
+    #pragma omp parallel for reduction(+:sum)
+    for (i = 0; i < num_steps; i++) {
+        double x = (i + 0.5) * step;
+        sum = sum + 4.0 / (1.0 + x * x);
     }
-    
 
-    pi = step * (sum1 + sum2);
+    pi = step * sum;
     auto end_time = std::chrono::high_resolution_clock::now();
     auto runtime = std::chrono::duration_cast<std::chrono::seconds> (end_time - start_time);
-    
+
     std::cout << "O valor de pi calculado com " << num_steps << " passos levou ";
     std::cout << runtime.count() << " segundo(s) e chegou no valor : ";
     std::cout.precision(17);
